d788a67c4cf26b82f5eb009b7be0c7441c0153fe
vigneshru96 <vigneshru96@gmail.com>
2022-08-20 00:23:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Vignesh and Himani....!</p>
+        <p>Vignesh and Himani ....!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
